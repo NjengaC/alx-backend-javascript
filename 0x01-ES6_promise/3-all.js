29b47bb6924722ace4775aff038be15cf0fd5e5d
NjengaC <victorcyrus01@gmail.com>
2024-05-29 15:31:59 +0300

    simpler

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
     })
-    .catch(() => {
-      console.log("Signup system offline");
-    });
+    .catch(() => console.log('Signup system offline'));
 }
+
+export default handleProfileSignup;
