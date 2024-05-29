92a284f874b5c5380e4d34923ca1b44df4e8176d
NjengaC <victorcyrus01@gmail.com>
2024-05-29 15:04:59 +0300

    then

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,5 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Response from API");
-    }, 1000);
+    resolve();
   });
-};
+}
