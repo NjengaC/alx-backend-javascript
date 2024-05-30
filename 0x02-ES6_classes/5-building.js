9595e27e680c5c2bfda8270149ccee823fc82ce2
NjengaC <victorcyrus01@gmail.com>
2024-05-30 21:04:59 +0300

    dangle

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,24 @@
-export default class Building {
-  constructor(sqft) {
-    if (this.constructor !== Building && typeof this.evacuationWarningMesagge !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw TypeError('Floors must be a number');
     }
-    this._sqft = sqft;
+    this._floors = newFloors;
   }
 
-  get sqft() {
-    return this._sqft;
-  }
-
-  set sqft(sqft) {
-    this._sqft = sqft;
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
