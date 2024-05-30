9a58ea4030f0714e60b4c990fc6e82de6b1ecd4c
NjengaC <victorcyrus01@gmail.com>
2024-05-30 21:41:02 +0300

    comments

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,22 +1,40 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
+  /**
+   * Constructs a Sky High Building object with a specified square foot and floors.
+   * @param {number} sqft - The square footage of the building.
+   * @param {number} floors - The number of floors in the building
+   * @throws {TypeError} if the floors is infinite or not a number
+   */
   constructor(sqft, floors) {
     super(sqft);
+    if (typeof floors !== 'number' || !Number.isFinite(floors)) throw new TypeError('Floors must be a number');
+
     this._floors = floors;
   }
 
+  /**
+   * Gets the number of floors of the building.
+   * @returns {number} The number of floors of the building.
+   */
   get floors() {
     return this._floors;
   }
 
-  set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw TypeError('Floors must be a number');
-    }
-    this._floors = newFloors;
+  /**
+   * Sets the number of floors of the building.
+   * @param {number} floors - The new number of floors of the building.
+   * @throws {TypeError} - If floors is not a finite number.
+   */
+  set floors(floors) {
+    if (typeof floors !== 'number' || !Number.isFinite(floors)) throw new TypeError('Floors must be a number');
+    this._floors = floors;
   }
 
+  /**
+   * @returns {string} The number of floors in the building
+   */
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
