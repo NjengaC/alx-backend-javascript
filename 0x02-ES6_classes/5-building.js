19231fb4100b6a7b2440c53acc8b542ddbf3f3a8
NjengaC <victorcyrus01@gmail.com>
2024-05-30 21:39:43 +0300

    news 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,10 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && typeof this.evacuationWarningMesagge !== 'function') {
+    if (typeof sqft !== 'number' || !Number.isFinite(sqft)) throw new TypeError('Square foot must be a number');
+    if (this.evacuationWarningMessage === undefined && this.constructor !== Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+
     this._sqft = sqft;
   }
 
@@ -11,6 +13,7 @@ export default class Building {
   }
 
   set sqft(sqft) {
+    if (typeof sqft !== 'number' || !Number.isFinite(sqft)) throw new TypeError('Square foot must be a number');
     this._sqft = sqft;
   }
 }
