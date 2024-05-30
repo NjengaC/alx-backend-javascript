b8fb0de4350121e10baf5f399e84f1d0d7cc0671
NjengaC <victorcyrus01@gmail.com>
2024-05-30 21:14:46 +0300

    super

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,13 +1,19 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
+    if (typeof floors !== 'number' || !Number.isFinite(floors)) throw new TypeError('Floors must be a number');
+
     this._floors = floors;
   }
 
   get floors() {
     return this._floors;
+  }
+  set floors(floors) {
+    if (typeof floors !== 'number' || !Number.isFinite(floors)) throw new TypeError('Floors must be a number');
+    this._floors = floors;
   }
 
   evacuationWarningMessage() {
